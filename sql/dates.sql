42762f62d1e9319d28d8c956a34094b802c5bf58
unchartech_2 <mateo.rodriguez@unchartech.com>
2019-02-06 18:58:12 +0100

    sql request for min max dates

---
+++ sql/dates.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE VIEW ciq.min_max_dates AS
+SELECT * 
+FROM
+(
+SELECT ticker_bbg
+, concat('{"min_date": "', min(period_date), '", "max_date": "', max(period_date), '"}') AS statements_standard_year
+FROM ciq.statements_standard_year
+GROUP BY ticker_bbg
+) AS y
+FULL OUTER JOIN
+(
+SELECT ticker_bbg
+, concat('{"min_date": "', min(period_date), '", "max_date": "', max(period_date), '"}') AS statements_standard_quarter
+FROM ciq.statements_standard_quarter
+GROUP BY ticker_bbg
+) AS q
+USING(ticker_bbg)
